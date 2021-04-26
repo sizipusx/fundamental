a9de7d42947f69fe4c95490f0b4bf3aa08db8192
sizipusx <76734279+sizipusx@users.noreply.github.com>
2021-04-26 18:35:32 +0900

    Add files via upload

---
+++ drawAPT.py
@@ -0,0 +1,231 @@
+import time
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+
+import requests
+import json
+from pandas.io.json import json_normalize
+
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+import streamlit as st
+import FinanceDataReader as fdr
+
+# 챠트 기본 설정 
+# marker_colors = ['#34314c', '#47b8e0', '#ffc952', '#ff7473']
+marker_colors = ['rgb(27,38,81)', 'rgb(205,32,40)', 'rgb(22,108,150)', 'rgb(255,69,0)', 'rgb(237,234,255)']
+template = 'seaborn' #"plotly", "plotly_white", "plotly_dark", "ggplot2", "seaborn", "simple_white", "none".
+
+def run_price_index(selected_city2, mdf,jdf, mdf_change, jdf_change, bubble_df2, m_power) :
+   
+    titles = dict(text= '('+selected_city2 +') 주간 매매-전세 지수', x=0.5, y = 0.9) 
+
+    fig = make_subplots(specs=[[{'secondary_y': True}]]) 
+    
+    fig.add_trace(go.Bar(name = '매매지수증감', x = mdf.index, y = mdf_change[selected_city2].round(decimals=2), marker_color=  marker_colors[2]), secondary_y = True)
+    fig.add_trace(go.Bar(name = '전세지수증감', x = jdf.index, y = jdf_change[selected_city2].round(decimals=2), marker_color=  marker_colors[1]), secondary_y = True)
+
+    
+    fig.add_trace(go.Scatter(mode='lines', name = '매매지수', x =  mdf.index, y= mdf[selected_city2], marker_color = marker_colors[0]), secondary_y = False)
+    fig.add_trace(go.Scatter(mode='lines', name ='전세지수', x =  jdf.index, y= jdf[selected_city2], marker_color = marker_colors[3]), secondary_y = False)
+    fig.update_layout(hovermode="x unified")
+    # fig.update_xaxes(showspikes=True, spikecolor="green", spikesnap="cursor", spikemode="across", spikethickness=0.5)
+    fig.update_yaxes(showspikes=True)#, spikecolor="orange", spikethickness=0.5)
+    fig.update_yaxes(title_text='지수', showticklabels= True, showgrid = True, zeroline=False,  secondary_y = False) #ticksuffix="%"
+    fig.update_yaxes(title_text='지수 증감', showticklabels= True, showgrid = False, zeroline=True, zerolinecolor='LightPink', secondary_y = True, ticksuffix="%") #tickprefix="$", 
+    fig.update_layout(title = titles, titlefont_size=15, legend=dict(orientation="h"), template=template)
+    fig.update_layout(
+            showlegend=True,
+            legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ),
+            xaxis=go.layout.XAxis(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=6,
+                        label="6m",
+                        step="month",
+                        stepmode="backward"),
+                    dict(count=1,
+                        label="YTD",
+                        step="year",
+                        stepmode="todate"),
+                    dict(count=1,
+                        label="1y",
+                        step="year",
+                        stepmode="backward"),
+                    dict(count=5,
+                        label="5y",
+                        step="year",
+                        stepmode="backward"),
+                    dict(count=10,
+                        label="10y",
+                        step="year",
+                        stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+            )      
+        )
+    st.plotly_chart(fig)
+
+    #bubble index chart
+    titles = dict(text= '('+selected_city2 +') 월간 버블 지수', x=0.5, y = 0.9) 
+
+    fig = make_subplots(specs=[[{'secondary_y': True}]]) 
+    
+    # fig.add_trace(go.Bar(name = '매매지수증감', x = mdf.index, y = mdf_change[selected_city2].round(decimals=2), marker_color=  marker_colors[2]), secondary_y = True)
+    # fig.add_trace(go.Bar(name = '전세지수증감', x = jdf.index, y = jdf_change[selected_city2].round(decimals=2), marker_color=  marker_colors[1]), secondary_y = True)
+
+    
+    fig.add_trace(go.Scatter(mode='lines', name = '버블지수', x =  bubble_df2.index, y= bubble_df2[selected_city2], marker_color = marker_colors[0]), secondary_y = True)
+    fig.add_trace(go.Scatter(mode='lines', name ='전세파워', x =  m_power.index, y= m_power[selected_city2], marker_color = marker_colors[3]), secondary_y = False)
+    fig.update_layout(hovermode="x unified")
+    # fig.update_xaxes(showspikes=True, spikecolor="green", spikesnap="cursor", spikemode="across", spikethickness=0.5)
+    fig.update_yaxes(showspikes=True)#, spikecolor="orange", spikethickness=0.5)
+    fig.update_yaxes(title_text='전세파워', showticklabels= True, showgrid = False, zeroline=True, zerolinecolor='red',  secondary_y = False) #ticksuffix="%"
+    fig.update_yaxes(title_text='버블지수', showticklabels= True, showgrid = True, zeroline=True, zerolinecolor='blue', secondary_y = True) #tickprefix="$", 
+    fig.update_layout(title = titles, titlefont_size=15, legend=dict(orientation="h"), template=template)
+    st.plotly_chart(fig)
+
+    #box chart
+    fig2 = go.Figure()
+    title = '('  + selected_city2 + ') Index Change Statistics'
+    titles = dict(text= title, x=0.5, y = 0.9) 
+    fig2.add_trace(go.Box(x=mdf_change.loc[:,selected_city2], name='Index Change', boxpoints='all', marker_color = 'indianred',
+                    boxmean='sd', jitter=0.3, pointpos=-1.8 ))
+    fig2.update_layout(title = titles, titlefont_size=15, legend=dict(orientation="h"), template=template)
+    # fig2.add_trace(go.Box(x=earning_df.loc[:,'EPS Change'], name='EPS Change'))
+    st.plotly_chart(fig2)
+
+def run_sentimental_index():
+    # st.dataframe(senti_df)
+     # 챠트 기본 설정 
+    # marker_colors = ['#34314c', '#47b8e0', '#ffc952', '#ff7473']
+    marker_colors = ['rgb(27,38,81)', 'rgb(205,32,40)', 'rgb(22,108,150)', 'rgb(255,255,255)', 'rgb(237,234,255)']
+    template = 'seaborn' #"plotly", "plotly_white", "plotly_dark", "ggplot2", "seaborn", "simple_white", "none".
+
+    titles = dict(text= ' 매수매도 우위 지수('+ selected_dosi + ')', x=0.5, y = 0.9) 
+
+    fig = make_subplots(specs=[[{'secondary_y': True}]]) 
+    fig.add_trace(go.Scatter(line = dict(dash='dash'), name = '매도자 많음', x =  js_1.index, y= js_1[selected_dosi], marker_color = marker_colors[0]), secondary_y = False)
+    fig.add_trace(go.Scatter(line = dict(dash='dot'), name ='매수자 많음', x =  js_2.index, y= js_2[selected_dosi], marker_color = marker_colors[2]), secondary_y = False)                                             
+    fig.add_trace(go.Scatter(mode='lines', name ='매수매도 지수', x =  js_index.index, y= js_index[selected_dosi], marker_color = marker_colors[1]), secondary_y = False)
+    fig.update_layout(hovermode="x unified")
+    # fig.update_xaxes(showspikes=True, spikecolor="green", spikesnap="cursor", spikemode="across", spikethickness=0.5)
+    fig.update_yaxes(showspikes=True) #, spikecolor="orange", spikethickness=0.5)
+    fig.update_yaxes(title_text='지수', showticklabels= True, showgrid = True, zeroline=True, zerolinecolor='LightPink', secondary_y = False) #ticksuffix="%"
+    fig.update_layout(title = titles, titlefont_size=15, legend=dict(orientation="h"), template=template)
+    fig.add_hline(y=100.0, line_color="pink", annotation_text="100>매수자많음", annotation_position="bottom right")
+    fig.add_vrect(x0="2017-08-07", x1="2017-08-14", 
+              annotation_text="8.2 대책", annotation_position="top left",
+              fillcolor="green", opacity=0.25, line_width=0)
+    fig.add_vrect(x0="2018-09-17", x1="2018-10-01", 
+              annotation_text="9.13 대책", annotation_position="top left",
+              fillcolor="green", opacity=0.25, line_width=0)
+    fig.add_vrect(x0="2019-12-16", x1="2020-02-24", 
+              annotation_text="12.16/2.24 대책", annotation_position="top left",
+              fillcolor="green", opacity=0.25, line_width=0)
+    fig.add_vrect(x0="2020-06-22", x1="2020-07-13", 
+              annotation_text="6.17/7.10 대책", annotation_position="top left",
+              fillcolor="green", opacity=0.25, line_width=0)
+    fig.add_vrect(x0="2020-08-10", x1="2020-08-17", 
+              annotation_text="8.4 대책", annotation_position="bottom left",
+              fillcolor="green", opacity=0.25, line_width=0)
+    fig.update_layout(
+            showlegend=True,
+            legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ),
+            xaxis=go.layout.XAxis(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=6,
+                        label="6m",
+                        step="month",
+                        stepmode="backward"),
+                    dict(count=1,
+                        label="YTD",
+                        step="year",
+                        stepmode="todate"),
+                    dict(count=1,
+                        label="1y",
+                        step="year",
+                        stepmode="backward"),
+                    dict(count=5,
+                        label="5y",
+                        step="year",
+                        stepmode="backward"),
+                    dict(count=10,
+                        label="10y",
+                        step="year",
+                        stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+            )      
+        )
+    st.plotly_chart(fig)
+
+def draw_basic(last_df):
+    title = dict(text='주요 시 구 주간 매매지수 증감',  x=0.5, y = 0.9) 
+    template = 'seaborn' #"plotly", "plotly_white", "plotly_dark", "ggplot2", "seaborn", "simple_white", "none".
+    fig = px.bar(last_df, x= last_df.index, y=last_df.iloc[:,0], color=last_df.iloc[:,0], color_continuous_scale='Bluered', \
+                text=last_df.index)
+    fig.update_layout(title = title, titlefont_size=15, legend=dict(orientation="h"), template=template)
+    fig.update_traces(texttemplate='%{label}', textposition='outside')
+    fig.update_layout(uniformtext_minsize=6, uniformtext_mode='show')
+    fig.update_yaxes(title_text='주간 매매지수 증감률', showticklabels= True, showgrid = True, zeroline=True, zerolinecolor='LightPink', ticksuffix="%")
+    fig.add_hline(y=last_df.iloc[0,0], line_dash="dash", line_color="red", annotation_text=f"전국 증감률: {round(last_df.iloc[0,0],2)}", \
+                annotation_position="bottom right")
+    st.plotly_chart(fig)
+    st.dataframe(last_df.T.style.highlight_max(axis=1))
+
+    with st.beta_container():
+        #매매/전세 증감률 Bubble Chart
+        title = dict(text='주요 시 구 주간 매매/전세지수 증감', x=0.5, y = 0.9) 
+        fig1 = px.scatter(last_df, x='매매증감', y='전세증감', color='매매증감', size=abs(last_df['전세증감']), 
+                            text= last_df.index, hover_name=last_df.index, color_continuous_scale='Bluered')
+        fig1.update_yaxes(zeroline=True, zerolinecolor='LightPink', ticksuffix="%")
+        fig1.update_xaxes(zeroline=True, zerolinecolor='LightPink', ticksuffix="%")
+        fig1.update_layout(title = title, titlefont_size=15, legend=dict(orientation="h"), template=template)
+        st.plotly_chart(fig1)
+
+def draw_pir(selected_city2, pir_df, income_df, price_df):
+    titles = dict(text= '('+selected_city2 +') 분기 PIR 지수', x=0.5, y = 0.9) 
+
+    fig = make_subplots(specs=[[{'secondary_y': True}]]) 
+    
+    fig.add_trace(go.Bar(name = '가구소득', x = income_df.index, y = income_df[selected_city2], marker_color=  marker_colors[2]), secondary_y = True)
+    fig.add_trace(go.Bar(name = '주택가격', x = price_df.index, y = price_df[selected_city2], marker_color=  marker_colors[1]), secondary_y = True)
+
+    
+    fig.add_trace(go.Scatter(mode='lines', name = 'PIR', x =  pir_df.index, y= pir_df[selected_city2], marker_color = marker_colors[0]), secondary_y = False)
+    # fig.add_trace(go.Scatter(mode='lines', name ='전세지수', x =  jdf.index, y= jdf[selected_city2], marker_color = marker_colors[3]), secondary_y = False)
+    fig.update_layout(hovermode="x unified")
+    # fig.update_xaxes(showspikes=True, spikecolor="green", spikesnap="cursor", spikemode="across", spikethickness=0.5)
+    fig.update_yaxes(showspikes=True)#, spikecolor="orange", spikethickness=0.5)
+    fig.update_yaxes(title_text='PIR', showticklabels= True, showgrid = True, zeroline=False,  secondary_y = False) #ticksuffix="%"
+    fig.update_yaxes(title_text='가구소득-주택가격', showticklabels= True, showgrid = False, zeroline=True, zerolinecolor='LightPink', secondary_y = True, ticksuffix="만원") #tickprefix="$", 
+    fig.update_layout(title = titles, titlefont_size=15, legend=dict(orientation="h"), template=template)
+    st.plotly_chart(fig)
---
+++ kb_month.py
@@ -0,0 +1,249 @@
+import time
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+
+import requests
+import json
+from pandas.io.json import json_normalize
+
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+import streamlit as st
+import FinanceDataReader as fdr
+
+import drawAPT
+
+
+pd.set_option('display.float_format', '{:.2f}'.format)
+#오늘날짜까지
+now = datetime.now()
+today = '%s-%s-%s' % ( now.year, now.month, now.day)
+
+file_path = 'G:/내 드라이브/code/data/★(월간)KB주택가격동향_시계열(2021.04)_A지수통계.xlsx'
+
+@st.cache
+def load_index_data():
+    kbm_dict = pd.read_excel(file_path, sheet_name=None, header=1)
+
+    #헤더 변경
+    path = r'G:/내 드라이브/code/data/KB헤더.xlsx'
+    data_type = 'KB시도구' 
+    header = pd.read_excel(path, sheet_name=data_type)
+
+    #주택가격지수
+    mdf = kbm_dict['매매APT']
+    jdf = kbm_dict['전세APT']
+    mdf.columns = header.columns
+    mdf = mdf.drop([0,1])
+    jdf.columns = header.columns
+    jdf = jdf.drop([0,1])
+    #index 날짜 변경
+    index_list = list(mdf["KB주간"])
+
+    new_index = []
+
+    for num, raw_index in enumerate(index_list):
+        temp = str(raw_index).split('.')
+        if int(temp[0]) > 12 :
+            if len(temp[0]) == 2:
+                new_index.append('19' + temp[0] + '.' + temp[1])
+            else:
+                new_index.append(temp[0] + '.' + temp[1])
+        else:
+            new_index.append(new_index[num-1].split('.')[0] + '.' + temp[0])
+
+    mdf.set_index(pd.to_datetime(new_index), inplace=True)
+    jdf.set_index(pd.to_datetime(new_index), inplace=True)
+    mdf.drop('KB주간', axis=1, inplace=True)
+    jdf.drop('KB주간', axis=1, inplace=True)
+    mdf = mdf.round(decimals=2)
+    jdf = jdf.round(decimals=2)
+   
+    return mdf, jdf
+
+@st.cache
+def load_senti_data():
+    kb_dict = pd.read_excel(file_path, sheet_name=None, header=1)
+
+    js = kb_dict['매수매도']
+    js = js.set_index("Unnamed: 0")
+    js.index.name="날짜"
+
+    #컬럼명 바꿈
+    j1 = js.columns.map(lambda x: x.split(' ')[0])
+
+    new_s1 = []
+    for num, gu_data in enumerate(j1):
+        check = num
+        if gu_data.startswith('Un'):
+            new_s1.append(new_s1[check-1])
+        else:
+            new_s1.append(j1[check])
+
+    #컬럼 설정
+    js.columns = [new_s1,js.iloc[0]]
+    js = js.round(decimals=2)
+
+    return js
+
+@st.cache
+def load_pir_data():
+    kb_dict = pd.read_excel(file_path, sheet_name=None, header=1)
+    pir =  kb_dict['KB아파트담보대출PIR']
+    pir = pir.iloc[:pir['지역'].count()-1,1:11]
+
+    s1 = ['분기', '서울', '서울', '서울', '경기', '경기', '경기', '인천', '인천', '인천']
+    s2 = pir.iloc[0]
+    pir.columns = [s1, s2]
+    pir = pir.iloc[1:]
+    pir = pir.set_index(('분기',            '년도'))
+    pir.index.name = '분기'
+    #분기 날짜 바꾸기
+    pir_index = list(pir.index)
+    new_index = []
+
+    for num, raw_index in enumerate(pir_index):
+        temp = str(raw_index).split(' ')
+        if len(temp[0]) == 3:
+            if int(temp[0].replace("'","")) >84:
+                new_index.append('19' + temp[0].replace("'","") + '.' + temp[1])
+            else:
+                if temp[1] == '1Q':
+                    new_index.append('20' + temp[0].replace("'","") + '.03')
+                elif temp[1] == '2Q':
+                    new_index.append('20' + temp[0].replace("'","") + '.06')
+                elif temp[1] == '3Q':
+                    new_index.append('20' + temp[0].replace("'","") + '.09')
+                else:
+                    new_index.append('20' + temp[0].replace("'","") + '.12')
+        else:
+            if temp[0] == '1Q':
+                new_index.append(new_index[num-1].split('.')[0] + '.03')
+            elif temp[0] == '2Q':
+                new_index.append(new_index[num-1].split('.')[0] + '.06')
+            elif temp[0] == '3Q':
+                new_index.append(new_index[num-1].split('.')[0] + '.09')
+            else:
+                new_index.append(new_index[num-1].split('.')[0] + '.12')
+    ###각 지역 pir만
+    pir_df = pir.xs("KB아파트 PIR", axis=1, level=1)
+    pir_df.set_index(pd.to_datetime(new_index), inplace=True)
+    ###가구소득
+    income_df = pir.xs("가구소득(Income)", axis=1, level=1)
+    income_df.set_index(pd.to_datetime(new_index), inplace=True)
+    ###주택가격
+    house_df = pir.xs("주택가격\n(Price)", axis=1, level=1)
+    house_df.set_index(pd.to_datetime(new_index), inplace=True)
+    pir_df.index.name = '분기'
+    income_df.index.name = '분기'
+    house_df.index.name = '분기'
+
+    return pir_df, income_df, house_df
+
+
+
+
+if __name__ == "__main__":
+    st.title("KB 부동산 월간 시계열 분석")
+    data_load_state = st.text('Loading 매매/전세 index Data...')
+    mdf, jdf = load_index_data()
+    data_load_state.text("매매/전세 index Data Done! (using st.cache)")
+
+    #월간 증감률
+    mdf_change = mdf.pct_change()*100
+    mdf_change = mdf_change.astype(float)
+    mdf_change = mdf_change.iloc[1:]
+    jdf_change = jdf.pct_change()*100
+    jdf_change = jdf_change.iloc[1:]
+    jdf_change = jdf_change.astype(float)
+    #일주일 간 상승률 순위
+    last_df = mdf_change.iloc[-1].T.to_frame()
+    last_df['전세증감'] = jdf_change.iloc[-1].T.to_frame()
+    last_df.columns = ['매매증감', '전세증감']
+    last_df.dropna(inplace=True)
+    last_df = last_df.round(decimals=2)
+
+    #버블 지수 만들어 보자
+    # st.dataframe(mdf_change)
+    # st.dataframe(jdf_change)
+    #아기곰 방식:버블지수 =(관심지역매매가상승률-전국매매가상승률) - (관심지역전세가상승률-전국전세가상승률)
+    bubble_df = mdf_change.subtract(mdf_change['전국'], axis=0)- jdf_change.subtract(jdf_change['전국'], axis=0)
+    bubble_df = bubble_df*100
+    
+    #곰곰이 방식: 버블지수 = 매매가비율(관심지역매매가/전국평균매매가) - 전세가비율(관심지역전세가/전국평균전세가)
+    bubble_df2 = mdf.div(mdf['전국'], axis=0) - jdf.div(jdf['전국'], axis=0)
+    bubble_df2 = bubble_df2.round(decimals=5)*100
+    # st.dataframe(bubble_df2)
+
+    #전세 파워 만들기
+    cum_ch = (mdf_change/100 +1).cumprod()
+    jcum_ch = (jdf_change/100 +1).cumprod()
+    m_power = (jcum_ch - cum_ch)*100
+    
+
+    #여기서부터는 선택
+    my_choice = st.sidebar.radio(
+                    "What' are you gonna do?", ('Basic','Price Index', 'PIR', 'Sentiment analysis')
+                    )
+    if my_choice == 'Basic':
+        submit = st.sidebar.button('Draw Basic chart')
+        if submit:
+            drawAPT.draw_basic(last_df)
+
+    elif my_choice == 'Price Index':
+
+        city_list = ['전국', '서울', '6개광역시','부산','대구','인천','광주','대전','울산','5개광역시','수도권','세종','경기', '수원', \
+                    '성남','고양', '안양', '부천', '의정부', '광명', '평택','안산', '과천', '구리', '남양주', '용인', '시흥', '군포', \
+                    '의왕','하남','오산','파주','이천','안성','김포', '양주','동두천','경기광주', '화성','강원', '춘천','강릉', '원주', \
+                    '충북','청주', '충주','제천', '충남','천안', '공주','아산', '논산', '계룡','당진','서산', '전북', '전주', '익산', '군산', \
+                    '전남', '목포','순천','여수','광양','경북','포항','구미', '경산', '안동','김천','경남','창원', '양산','거제','진주', \
+                    '김해','통영', '제주도','제주서귀포','기타지방']
+        column_list = mdf.columns.to_list()
+        city_series = pd.Series(column_list)
+        selected_city = st.sidebar.selectbox(
+                '광역시-도-시', city_list
+            )
+        second_list = city_series[city_series.str.contains(selected_city)].to_list()
+        selected_city2 = st.sidebar.selectbox(
+                '구-시', second_list
+            )
+        # if  st.checkbox('Show 매매지수 data'):
+        #     st.dataframe(mdf.style.highlight_max(axis=0))
+        
+        submit = st.sidebar.button('Draw Price Index chart')
+        if submit:
+            drawAPT.run_price_index(selected_city2, mdf, jdf, mdf_change, jdf_change, bubble_df2, m_power)
+    elif my_choice == 'PIR':
+        data_load_state = st.text('Loading PIR index Data...')
+        pir_df, income_df, price_df = load_pir_data()
+        data_load_state.text("PIR index Data Done! (using st.cache)")
+
+        city_list = ['서울', '경기', '인천']
+        selected_city = st.sidebar.selectbox(
+                '수도권', city_list
+            )
+        submit = st.sidebar.button('Draw PIR chart')
+        if submit:
+            drawAPT.draw_pir(selected_city, pir_df, income_df, price_df)
+    else:
+        data_load_state = st.text('Loading 매수매도 index Data...')
+        # senti_df = load_senti_data()
+        data_load_state.text("매수매도 index Data Done! (using st.cache)")
+
+        # js_1 = senti_df.xs("매도자많음", axis=1, level=1)
+        # js_2 = senti_df.xs("매수자많음", axis=1, level=1)
+        # js_index = senti_df.xs("매수우위지수", axis=1, level=1)
+
+        # # city_list = ['전국', '서울', '강북', '강남', '6개광역시','5개광역시','부산','대구','인천','광주','대전','울산',,'수도권','세종', \
+        # #             '경기도', '강원도', '충청북도', '전라북도', '전라남도', '경상북도','경상남도','기타지방','제주']
+        # column_list = js_index.columns.to_list()
+        # selected_dosi = st.sidebar.selectbox(
+        #         '광역시-도', column_list
+        #     )
+        # submit = st.sidebar.button('Draw Sentimental Index chart')
+        # if submit:
+        #     run_sentimental_index()
