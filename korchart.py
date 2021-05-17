24b9f497a7930d79e02355b41f72448e1d55de86
sizipusx <sizipusx2@gmail.com>
2021-05-17 22:24:04 +0900

    chajgne

--- korchart.py
+++ korchart.py
@@ -71,8 +71,8 @@ def run(code, com_name):
         mode = "gauge+number+delta",
         value = ttm_df.iloc[-1,0],
         delta = {'reference': ttm_df['EPS'].max(), 'relative': True},
-        title = {'text': f"Last EPS (r={ttm_df.iloc[-1]}) relative Max EPS"},
-        domain = {'x': [0, 1], 'y': [0, 1]}
+        title = {'text': f"Last EPS (r={ttm_df.iloc[-1,0]}) relative Max EPS"},
+        domain = {'x': [0, ttm_df['EPS'].max()], 'y': [0, ttm_df['EPS'].max()]}
     ))
     st.plotly_chart(fig)
 
