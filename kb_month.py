7d83d2404b6ca3195ac7a80efff6a7886d0c2ed6
sizipusx <sizipusx2@gmail.com>
2021-05-18 23:00:36 +0900

    hg

--- kb_month.py
+++ kb_month.py
@@ -62,7 +62,7 @@ def load_buy_data():
     df = df.astype(float)
     org_df = df.copy()
     st.dataframe(df)
-    drop_list = ['전국', '서울', '경기', '경북', '경남', '전남', '전북', '강원', '대전', '대구', '인천', '광주', '부산', '울산', '세종','충남', '충북']
+    drop_list = ('전국', '서울', '경기', '경북', '경남', '전남', '전북', '강원', '대전', '대구', '인천', '광주', '부산', '울산', '세종','충남', '충북')
     df.drop(drop_list, axis=1, inplace=True)
     # df = df[df.columns[~df.columns.get_level_values(0).str.endswith('군')]]
 
