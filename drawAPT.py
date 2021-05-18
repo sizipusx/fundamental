16bcfca5b08534e20298b8f55bf6c847020ef4b5
sizipusx <sizipusx2@gmail.com>
2021-05-18 23:48:50 +0900

    dfd

--- drawAPT.py
+++ drawAPT.py
@@ -677,7 +677,6 @@ def run_buy_basic(b_df, org_df):
     fig.update_layout(title = titles, uniformtext_minsize=8, uniformtext_mode='hide')
     st.plotly_chart(fig)
 
-    st.dataframe(df_outer.iloc[-1])
     fig = px.bar(df_outer, x= df_outer.index, y=df_outer.iloc[:,0], color=df_outer.iloc[:,0], color_continuous_scale='Bluered', text=df_outer.indexss)
     fig.update_layout(title = titles, titlefont_size=15, legend=dict(orientation="h"), template=template)
     fig.update_traces(texttemplate='%{label}', textposition='outside')
