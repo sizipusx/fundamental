49e6dd3efd895c26eaa97219ceb4d380768693d3
indiesoul2@gmail.com <sizipusx2@gmail.com>
2021-07-21 15:14:53 +0900

    우리나라 지도 update

--- kb_house_chart.py
+++ kb_house_chart.py
@@ -16,6 +16,10 @@ from plotly.subplots import make_subplots
 import streamlit as st
 import FinanceDataReader as fdr
 
+import matplotlib.pyplot as plt
+
+plt.rc('font', family='NanumBarunGothic') 
+
 
 pd.set_option('display.float_format', '{:.2f}'.format)
 #오늘날짜까지
@@ -324,6 +328,86 @@ def draw_basic():
                 annotation_position="bottom right")
     st.plotly_chart(fig)
     # st.datafrasme(last_df.T.style.highlight_max(axis=1))
+
+    #2021-7-21 update 개별로
+def drawKorea(targetData, blockedMap, d1, d2, cmapname):
+    gamma = 0.75
+
+    whitelabelmin = (max(blockedMap[targetData]) - min(blockedMap[targetData])) * 0.25 + min(blockedMap[targetData])
+
+    datalabel = targetData
+
+    vmin = min(blockedMap[targetData])
+    vmax = max(blockedMap[targetData])
+
+    BORDER_LINES = [
+        [(1,0),(1,1),(2,1),(2,2),(6,2),(6,0),(1,0)], # 인천
+        [(1, 5), (1, 7), (2, 5), (3, 5), (3, 4), (8, 4), (8, 7), (7, 7), (7, 9), (4, 9), (4, 7), (1, 7),(2,5)], # 서울
+        [(1, 9), (3, 9), (3, 10), (8, 10), (8, 9),
+         (9, 9), (9, 8), (10, 8), (10, 5), (9, 5), (9, 3)], # 경기도
+        [(9, 12), (9, 10), (8, 10)], # 강원도
+        [(10, 5), (11, 5), (11, 4), (12, 4), (12, 5), (13, 5),
+         (13, 4), (14, 4), (14, 2)], # 충청남도
+        [(11, 5), (12, 5), (12, 6), (15, 6), (15, 7), (13, 7),
+         (13, 8), (11, 8), (11, 9), (10, 9), (10, 8)], # 충청북도
+        [(14, 4), (15, 4), (15, 6)], # 대전시
+        [(14, 7), (14, 9), (13, 9), (13, 11), (13, 13)], # 경상북도
+        [(14, 8), (16, 8), (16, 10), (15, 10),
+         (15, 11), (14, 11), (14, 12), (13, 12)], # 대구시
+        [(15, 11), (16, 11), (16, 13)], # 울산시
+        [(17, 1), (17, 3), (18, 3), (18, 6), (15, 6)], # 전라북도
+        [(19, 2), (19, 4), (21, 4), (21, 3), (22, 3), (22, 2), (19, 2)], # 광주시
+        [(18, 5), (20, 5), (20, 6)], # 전라남도
+        [(16, 9), (18, 9), (18, 8), (19, 8), (19, 9), (20, 9), (20, 10)], # 부산시
+    ]
+
+    mapdata = blockedMap.pivot(index='y', columns='x', values=targetData)
+    masked_mapdata = np.ma.masked_where(np.isnan(mapdata), mapdata)
+    
+    plt.figure(figsize=(8, 13))
+    plt.pcolor(masked_mapdata, vmin=vmin, vmax=vmax, cmap=cmapname, edgecolor='#aaaaaa', linewidth=0.5)
+
+    # 지역 이름 표시
+    for idx, row in blockedMap.iterrows():
+        annocolor = 'white' if row[targetData] > whitelabelmin else 'black'
+
+        # 광역시는 구 이름이 겹치는 경우가 많아서 시단위 이름도 같이 표시한다. (중구, 서구)
+        if row[d1].endswith('시') and not row[d1].startswith('세종'):
+            dispname = '{}\n{}'.format(row[d1][:2], row[d2][:-1])
+            if len(row[d2]) <= 2:
+                dispname += row[d2][-1]
+        else:
+            dispname = row[d2][:-1]
+
+        # 서대문구, 서귀포시 같이 이름이 3자 이상인 경우에 작은 글자로 표시한다.
+        if len(dispname.splitlines()[-1]) >= 3:
+            fontsize, linespacing = 9.5, 1.5
+        else:
+            fontsize, linespacing = 11, 1.2
+
+        plt.annotate(dispname, (row['x']+0.5, row['y']+0.5), weight='bold',
+                     fontsize=fontsize, ha='center', va='center', color=annocolor,
+                     linespacing=linespacing)
+        
+    # 시도 경계 그린다.
+    for path in BORDER_LINES:
+        ys, xs = zip(*path)
+        plt.plot(xs, ys, c='black', lw=4)
+
+    plt.gca().invert_yaxis()
+    #plt.gca().set_aspect(1)
+
+    plt.axis('off')
+
+    cb = plt.colorbar(shrink=.1, aspect=10)
+    cb.set_label(datalabel)
+
+    plt.tight_layout()
+    plt.show()
+
+
+    #function 불러 보자
+    drawKorea('면적', df1, '광역시도', '행정구역', 'Blues')
 
     with st.beta_container():
         #매매/전세 증감률 Bubble Chart
