1aedfbdb1b5739b75a37b4d41c468ba62defa636
indiesoul2@gmail.com <sizipusx2@gmail.com>
2021-10-07 16:20:14 +0900

    detail change update

--- house_dashboard.py
+++ house_dashboard.py
@@ -549,35 +549,39 @@ html_br="""
 st.markdown(html_br, unsafe_allow_html=True)
 ### Block 6#########################################################################################
 with st.beta_container():
-    col2, col3, col4, col5, col6 = st.beta_columns([20,1,20,1,20])
+    col2, col3, col4 = st.beta_columns([30,2,30])
     with col2:
         drawAPT_update.run_sell_index(selected_city, peong_df, peong_ch)
     with col3:
         st.write("")
     with col4:
         drawAPT_update.run_jeon_index(selected_city, peongj_df, peongj_ch)
-    with col5:
-        st.write("")
-    with col6:
-        drawAPT_update.run_jeon_ratio(selected_city, ratio_df)
 
+st.markdown(html_br, unsafe_allow_html=True)
+### Block 6-1#########################################################################################
+with st.beta_container():
+    col2, col3, col4 = st.beta_columns([30,2,30])
+    with col2:
+        drawAPT_update.run_jeon_ratio(selected_city, ratio_df)
+    with col3:
+        st.write("")
+    with col4:
+        drawAPT_update.run_trade_index(selected_city, org_df, mdf)
+    
 html_br="""
 <br>
 """
 st.markdown(html_br, unsafe_allow_html=True)
 #################block 7###########################################################################
 with st.beta_container():
-    col2, col3, col4, col5, col6 = st.beta_columns([20,1,20,1,20])
+    col2, col3, col4 = st.beta_columns([30,2,30])
     with col2:
         drawAPT_update.run_buy_index(selected_city, org_df)
     with col3:
         st.write("")
     with col4:
         drawAPT_update.run_buy_ratio(selected_city, org_df)
-    with col5:
-        st.write("")
-    with col6:
-        drawAPT_update.run_trade_index(selected_city, org_df, mdf)
+
 html_br="""
 <br>
 """
