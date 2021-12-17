8363c4ddb95d459156540d24cbc36056adf2973e
indiesoul2@gmail.com <sizipusx2@gmail.com>
2021-12-17 09:33:15 +0900

    2021. 12. 13. 주간 업데이트

--- kb_house_chart.py
+++ kb_house_chart.py
@@ -35,7 +35,7 @@ now = datetime.now()
 today = '%s-%s-%s' % ( now.year, now.month, now.day)
 
 # file_path = 'G:/내 드라이브/code/data/WeeklySeriesTables(시계열)_20210419.xlsx'
-file_path = 'https://github.com/sizipusx/fundamental/blob/3d91c9c92ad560203d82ef91876ec61bc66fc08b/files/kb_weekly.xlsx?raw=True'
+file_path = 'https://github.com/sizipusx/fundamental/blob/c0d6a4c2f3e8f358df023cf0ccb9c8eee37a5fd9/files/kb_weekly.xlsx?raw=True'
 
 @st.cache
 def load_index_data():
