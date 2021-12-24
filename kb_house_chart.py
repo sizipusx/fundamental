2548fae205ee303de4320a35db89796c41f2fc37
indiesoul2@gmail.com <sizipusx2@gmail.com>
2021-12-24 15:37:02 +0900

    error

--- kb_house_chart.py
+++ kb_house_chart.py
@@ -464,6 +464,7 @@ def draw_basic():
             kb_last_df['1y'] = kb_last_df['1y'].rank(ascending=True, method='min').round(decimals=1)
             kb_last_df['매매증감'] = kb_last_df['매매증감'].round(decimals=2)
             kb_last_df['전세증감'] = kb_last_df['전세증감'].round(decimals=2)
+            st.write(kb_last_df['매매증감'])
             st.dataframe(kb_last_df)
             #drawAPT_weekly.draw_index_table(kb_last_df, flag)
 
