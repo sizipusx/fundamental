be0fec7f06c7a9666acb09a3096b8105db44cde0
indiesoul2@gmail.com <sizipusx2@gmail.com>
2021-12-24 15:45:14 +0900

    style 적용

--- kb_house_chart.py
+++ kb_house_chart.py
@@ -464,7 +464,8 @@ def draw_basic():
             kb_last_df['1y'] = kb_last_df['1y'].rank(ascending=True, method='min').round(decimals=1)
             kb_last_df['매매증감'] = round(kb_last_df['매매증감'], 2)
             kb_last_df['전세증감'] = kb_last_df['전세증감'].round(decimals=2)
-            st.write(type(kb_last_df['매매증감']))
+            kb_last_df.style.bar(subset=['1w,1y'],align='mid',color=['#a103fc','#03e3fc'])
+            kb_last_df.style.set_precision(2)
             st.dataframe(kb_last_df)
             #drawAPT_weekly.draw_index_table(kb_last_df, flag)
 
