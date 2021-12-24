d0a9ba0b4ba0f9e41fc8e716b9e69711454cc7ea
indiesoul2@gmail.com <sizipusx2@gmail.com>
2021-12-24 23:12:57 +0900

    test

--- kb_house_chart.py
+++ kb_house_chart.py
@@ -476,8 +476,9 @@ def draw_basic():
             kb_last_df['전세증감'] = kb_last_df['전세증감'].round(decimals=2)
             import seaborn as sns
             cmap = cmap=sns.diverging_palette(5, 250, as_cmap=True)
+            slice_ = ['1w%', '2w%', '3w%', '1m%', '1y%' ]
             st.dataframe(rank_df.style.background_gradient(cmap, axis=1)\
-                .format(precision=0, na_rep='MISSING', thousands=" ", formatter={'매매증감': "{:.2f}", '전세증감': "{:.2f}"})\
+                .format(precision=2, na_rep='MISSING', thousands=" ", subset=slice_)\
                 .bar(align='mid',color=['blue','red'])) 
             cm = sns.light_palette((260, 75, 60), input="husl", as_cmap=True)
             st.dataframe(rank_df.style.background_gradient(cmap=cm)\
