9ced38038fca3a50d30201f382f23ba8e3620050
indiesoul2@gmail.com <sizipusx2@gmail.com>
2021-12-25 20:23:31 +0900

    thsdfk

--- kb_house_chart.py
+++ kb_house_chart.py
@@ -681,7 +681,7 @@ if __name__ == "__main__":
         change_odf = pd.DataFrame()
         change_odf['매매증감'] = (slice_om.iloc[-1]/slice_om.iloc[0]-1).to_frame()
         change_odf['전세증감'] = (slice_oj.iloc[-1]/slice_oj.iloc[0]-1).to_frame()
-
+        change_odf = change_odf.dropna().round(decimals=2)
         st.dataframe(change_odf)
         submit = st.sidebar.button('Draw 기간 증감 chart')
         if submit:
