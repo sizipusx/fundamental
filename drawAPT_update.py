d06a2e4be4c207ee74215f945be0eb65b7cea742
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-01-05 21:00:11 +0900

    vuddfk

--- drawAPT_update.py
+++ drawAPT_update.py
@@ -554,8 +554,8 @@ def run_trade_index(selected_dosi, org_df, mdf):
                                     text= mdf[selected_dosi], textposition = 'top center', marker_color = marker_colors[0]),
                                     secondary_y = True)
     fig.update_traces(texttemplate='%{text:.3s}') 
-    fig.add_hline(y=selected_df['합계'].mean(axis=0), line_width=1, line_color="blue", line_dash="dot", 
-                        annotation_text=selected_df['합계'].mean(axis=0), annotation_position="bottom right")
+    fig.add_hline(y=round(selected_df['합계'].mean(axis=0),1), line_width=1, line_color="blue", line_dash="dot", 
+                        annotation_text=round(selected_df['합계'].mean(axis=0),1), annotation_position="bottom right")
     fig.update_yaxes(title_text="매매 거래량", showticklabels= True, showgrid = True, zeroline=True, ticksuffix="건", secondary_y = False)
     fig.update_yaxes(title_text="매매지수", showticklabels= True, showgrid = False, zeroline=True, secondary_y = True)
     fig.update_layout(title = titles, titlefont_size=15, legend=dict(orientation="h"), template=template, xaxis_tickformat = '%Y-%m')
