9ea930e92428d4dbc6c9222d880ad52a93951e23
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-01-19 22:36:13 +0900

    error change

--- house_dashboard.py
+++ house_dashboard.py
@@ -51,7 +51,7 @@ one_path = r'https://github.com/sizipusx/fundamental/blob/9ed78289936640f9bc210c
 buy_path = r'https://github.com/sizipusx/fundamental/blob/669cd865342b20c29da4ff689a309fe5edc24f38/files/apt_buy.xlsx?raw=true'
 # 2021. 11월부터 KB 데이터에서 기타지방 평균가격 제공하지 않음 => 다시 부동산원 데이터로 변경: 2021. 12. 16
 #p_path = r"https://github.com/sizipusx/fundamental/blob/85abf3c89fd35256caa84d3d216208408634686f/files/kb_price.xlsx?raw=True"
-p_path = r"https://github.com/sizipusx/fundamental/blob/bad11c793466a1fca828a13e03ad47acf4fe5738/files/one_apt_price.xlsx?raw=true'
+p_path = r"https://github.com/sizipusx/fundamental/blob/bad11c793466a1fca828a13e03ad47acf4fe5738/files/one_apt_price.xlsx?raw=true"
 pop_path = r"https://github.com/sizipusx/fundamental/blob/e946e69f2d27b84df736fecaf92b49d2089af0f9/files/pop.xlsx?raw=True"
 not_sell_path = 'https://github.com/sizipusx/fundamental/blob/8f2753b1fd827ced9fd20e11e6355756b6954657/files/not_selling_apt.xlsx?raw=true'
 #년단위
