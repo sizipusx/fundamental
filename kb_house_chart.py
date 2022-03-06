101c975b2b88a357e05318a4ef28b98ea0ea7349
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-03-06 20:46:51 +0900

    22.2.28.update

--- kb_house_chart.py
+++ kb_house_chart.py
@@ -87,9 +87,9 @@ now = datetime.now()
 today = '%s-%s-%s' % ( now.year, now.month, now.day)
 
 # file_path = 'G:/내 드라이브/code/data/WeeklySeriesTables(시계열)_20210419.xlsx'
-kb_file_path = r'https://github.com/sizipusx/fundamental/blob/1290cbafc35009486030fbed4753cefc22722d90/files/kb_weekly.xlsx?raw=True'
+kb_file_path = r'https://github.com/sizipusx/fundamental/blob/a8320e23cb02e17ee1f9b1221b92c0467ce8777a/files/kb_weekly.xlsx?raw=True'
 #감정원 데이터
-one_path = r"https://github.com/sizipusx/fundamental/blob/1290cbafc35009486030fbed4753cefc22722d90/files/one_weekly.xlsx?raw=True"
+one_path = r"https://github.com/sizipusx/fundamental/blob/a8320e23cb02e17ee1f9b1221b92c0467ce8777a/files/one_weekly.xlsx?raw=True"
 #헤더 변경
 header_path = r'https://github.com/sizipusx/fundamental/blob/00c7db01dd87012174224f5b9e89c24da4268d13/files/header.xlsx?raw=true'
 header_excel = pd.ExcelFile(header_path)
