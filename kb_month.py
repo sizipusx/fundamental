b7a326e329b9a7475e4a3935763658ef62247643
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-04-17 19:58:01 +0900

    서울거주자수

--- kb_month.py
+++ kb_month.py
@@ -461,6 +461,7 @@ if __name__ == "__main__":
     last_in = pd.DataFrame()
     last_in['전체거래수'] = total_df.iloc[-1].T.to_frame()
     last_in['투자자수'] = invest_total.iloc[-1].T.to_frame()
+    last_in['서울거주자수'] = seoul_buyer.iloc[-1].T.to_frame()
     last_in['투자자비율'] = in_ratio.iloc[-1].T.to_frame()
     last_in['비율평균'] = in_ratio.mean()
     ### 여기까지 매입자 거주지별 
