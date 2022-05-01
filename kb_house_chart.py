089be98d4d7d0352903245b4d93f755ca0943e8d
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-05-01 16:57:32 +0900

    aggr table change

--- kb_house_chart.py
+++ kb_house_chart.py
@@ -18,6 +18,8 @@ import plotly.io as pio
 from plotly.subplots import make_subplots
 
 import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
 import FinanceDataReader as fdr
 
 import matplotlib.pyplot as plt
@@ -96,6 +98,33 @@ header_excel = pd.ExcelFile(header_path)
 #geojson file open
 geo_source = r'https://raw.githubusercontent.com/sizipusx/fundamental/main/sigungu_json.geojson'
 
+@st.catch
+def aggrid_interactive_table(df: pd.DataFrame):
+    """Creates an st-aggrid interactive table based on a dataframe.
+
+    Args:
+        df (pd.DataFrame]): Source dataframe
+
+    Returns:
+        dict: The selected row
+    """
+    options = GridOptionsBuilder.from_dataframe(
+        df, enableRowGroup=True, enableValue=True, enablePivot=True
+    )
+
+    options.configure_side_bar()
+
+    options.configure_selection("single")
+    selection = AgGrid(
+        df,
+        enable_enterprise_modules=True,
+        gridOptions=options.build(),
+        theme="light",
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        allow_unsafe_jscode=True,
+    )
+
+    return selection
 
 @st.cache
 def get_basic_df():
