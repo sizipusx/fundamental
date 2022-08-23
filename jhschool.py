d37ba57d2d204c8df78fa07b07746f9e61bc3d8d
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-08-23 22:37:43 +0900

    chnge

--- jhschool.py
+++ jhschool.py
@@ -142,11 +142,12 @@ def run(g_status, gubun):
   if ok_st:
     #write_sheet.update_acell('B1', '이사없음')
     write_sheet.append_row([gubun, '이상 없음'])
+    st.subheader("확인이 완료 되었습니다!!")
     #write_sheet.insert_row(['new1', 'new2', 'new3', 'new4'], 5)
-  else:
+  if not_st:
     write_sheet.append_row([gubun, '이상 있음'])
+    st.subheader("이상이 있는 경우 담임선생님께 말씀 드리거나 담당선생님(윤대영T)께 말씀 드립니다.")
 
-  st.subheader("확인이 완료 되었습니다!! <br> 이상이 있는 경우 담임선생님께 말씀 드리거나 담당선생님(윤대영T)께 말씀 드립니다.")
 
 
 
