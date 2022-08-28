b203fbfc4ff02f0d38ddad46672e3e9ee93c37b0
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-08-28 17:20:43 +0900

    dfdaf

--- rebuild_house.py
+++ rebuild_house.py
@@ -210,7 +210,7 @@ if __name__ == "__main__":
     response  = aggrid_interactive_table(df=filter_df)
     if response:
         st.write("선택한 아파트 위치:")
-        st.dataframe(response["selected_rows"])
+        #st.dataframe(response["selected_rows"])
         selected_df = response["selected_rows"]
         px.set_mapbox_access_token(token)
         fig = px.scatter_mapbox(selected_df, lat="위도", lon="경도",     color="주거형태", size="공급면적", hover_name="단지명", hover_data=["특이사항", "한글거래가액", "시도"],
