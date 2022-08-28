249be5b5b6d812cd9390d8e2d332e0596081bee8
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-08-28 17:14:42 +0900

    dffsa

--- rebuild_house.py
+++ rebuild_house.py
@@ -202,6 +202,7 @@ if __name__ == "__main__":
     #if submit:
     show_local(city_name, city_apt, city_total)
     filter_df = city_total[['시도', '지역명', '단지명', '동', '매물방식', '주거형태', '공급면적', '전용면적', '층', '특이사항', '한글거래가액', '확인매물', '매물방향', '위도', '경도']]
+    filter_df['공급면적'] = filter_df['공급면적'].astype(int)
     response  = aggrid_interactive_table(df=filter_df)
     if response:
         st.write("You selected:")
