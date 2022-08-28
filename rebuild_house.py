21c971c31e776a70508199abef4e8700dbc3ac36
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-08-28 17:01:30 +0900

    dfdfadfadsf

--- rebuild_house.py
+++ rebuild_house.py
@@ -170,11 +170,11 @@ def show_local(select_city, city_apt, city_total):
     )
     st.plotly_chart(fig, use_container_width=True)
     st.write("단지명과 공급 면적에 따라 분류한 총 ("+ str(len(city_apt))+ " ) 개의 아파트가 있습니다.")  
-    filter_df = city_total[['시도', '지역명', '단지명', '동', '매물방식', '주거형태', '공급면적', '전용면적', '층', '특이사항', '한글거래가액', '확인매물', '매물방향']]
-    response  = aggrid_interactive_table(df=filter_df)
-    if response:
-        st.write("You selected:")
-        st.json(response["selected_rows"])
+    #filter_df = city_total[['시도', '지역명', '단지명', '동', '매물방식', '주거형태', '공급면적', '전용면적', '층', '특이사항', '한글거래가액', '확인매물', '매물방향']]
+    #response  = aggrid_interactive_table(df=filter_df)
+    #if response:
+    #    st.write("You selected:")
+    #    st.json(response["selected_rows"])
     
 
 
@@ -201,5 +201,10 @@ if __name__ == "__main__":
 
     if submit:
         show_local(city_name, city_apt, city_total)
+        filter_df = city_total[['시도', '지역명', '단지명', '동', '매물방식', '주거형태', '공급면적', '전용면적', '층', '특이사항', '한글거래가액', '확인매물', '매물방향']]
+        response  = aggrid_interactive_table(df=filter_df)
+        if response:
+            st.write("You selected:")
+            st.json(response["selected_rows"])
         
         
