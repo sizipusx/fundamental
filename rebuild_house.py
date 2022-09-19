75554be281c70636f30fce2102ca3ecd08a23d29
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-09-19 23:02:08 +0900

    앙랄ㅇ

--- rebuild_house.py
+++ rebuild_house.py
@@ -197,7 +197,7 @@ def load_data():
 def show_total(s_df):
     
     px.set_mapbox_access_token(token)
-    fig = px.scatter_mapbox(s_df, lat="위도", lon="경도",     color="주거형태", size="시세평균(만)", hover_name="단지명", hover_data=["물건수", "공급면적", "시도명"],
+    fig = px.scatter_mapbox(s_df, lat="위도", lon="경도",     color="주거형태", size="거래가(만원)", hover_name="단지명", hover_data=["물건수", "공급면적", "시도명"],
                     color_continuous_scale=px.colors.cyclical.IceFire, height=1000, size_max=30, zoom=10)
     fig.update_layout(
         title='전국 재건축-재개발 분양권 아파트 시세',
@@ -240,7 +240,7 @@ def show_total(s_df):
 
 def show_local(select_city, city_apt, city_total):
     px.set_mapbox_access_token(token)
-    fig = px.scatter_mapbox(city_apt, lat="위도", lon="경도",     color="주거형태", size="시세평균(만)", hover_name="단지명", hover_data=["물건수", "공급면적", "시도명"],
+    fig = px.scatter_mapbox(city_apt, lat="위도", lon="경도",     color="주거형태", size="거래가(만원)", hover_name="단지명", hover_data=["물건수", "공급면적", "시도명"],
                     color_continuous_scale=px.colors.cyclical.IceFire, size_max=30, zoom=10, height=800)
     fig.update_layout(
         title='[' + select_city+' ] 재건축-재개발 / 아파트 분양권 네이버 시세',
