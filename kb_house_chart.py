469e33b71278770fb87202c4049027bb813b3ff2
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-09-25 15:30:49 +0900

    img insert

--- kb_house_chart.py
+++ kb_house_chart.py
@@ -53,7 +53,7 @@ html_header="""
   border-width: 1.5px;"></h1>
 """
 
-st.set_page_config(page_title="주간 부동산 시계열 분석", page_icon="", layout="wide")
+st.set_page_config(page_title="주간 부동산 시계열 분석", page_icon="./files/15.png", layout="wide")
 st.markdown('<style>body{background-color: #fbfff0}</style>',unsafe_allow_html=True)
 st.markdown(html_header, unsafe_allow_html=True)
 st.markdown(""" <style>
