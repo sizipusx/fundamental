202b96de2efe18a2042510bf920402fc05cfd73e
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-10-09 15:35:16 +0900

    에러 변경

--- house_dashboard.py
+++ house_dashboard.py
@@ -375,6 +375,8 @@ def load_index_data():
     for query in query_list:
         df = pd.read_sql(query, conn, index_col='date')
         df.index = pd.to_datetime(df.index, format = '%Y-%m-%d')
+        df = df.apply(lambda x:x.replace('#DIV/0!','0')).apply(lambda x:x.replace('','0')).astype(float)
+        df = df.round(decimals=2)
         index_list.append(df)
     conn.close()
 
