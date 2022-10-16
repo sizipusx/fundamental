0aaaeb25489c84cdf348337d1bf64ca44eac2706
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-10-16 14:25:54 +0900

    dual

--- kb_house_chart.py
+++ kb_house_chart.py
@@ -124,6 +124,7 @@ def get_gsheet_index():
             )
         df = df.set_index(keys='날짜')
         df.index.name = 'date'
+        df.index = df.index.str.strip()
         df.index = pd.to_datetime(df.index, format = '%Y-%m-%d')
         df = df.apply(lambda x:x.replace('#DIV/0!','0')).apply(lambda x:x.replace('','0')).astype(float)
         df = df.round(decimals=2)
