9e32fd178c319750a4f6e1ef0cae2693bc5a5fa3
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-11-24 22:41:07 +0900

    markdown link

--- pages/📆_apt_weekly.py
+++ pages/📆_apt_weekly.py
@@ -1383,7 +1383,7 @@ html_line="""
 st.markdown(html_line, unsafe_allow_html=True)
 st.markdown(
 """
-- <p style="color:Gainsboro; text-align: right;">By: [기하급수적](https://blog.naver.com/indiesoul2) / sizipusx2@gmail.com </p>
+- <p style="color:Gainsboro; text-align: right;">By: [기하급수적]<https://blog.naver.com/indiesoul2> / <sizipusx2@gmail.com> </p>
 """)
 
 
