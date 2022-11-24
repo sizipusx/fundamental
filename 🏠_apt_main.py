7cf08f6b916b932c38653085c4ecf62cac3eb3a5
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-11-24 23:21:05 +0900

    메인 설명 추가

--- 🏠_apt_main.py
+++ 🏠_apt_main.py
@@ -15,12 +15,13 @@ st.markdown(
     Machine Learning and Data Science projects.
     **👈 Select a demo from the sidebar** to see some examples
     of what Streamlit can do!
-    ### Want to learn more?
+    ### Menu 설명
+    - apt weekly: 한국부동산원과 KB에서 매주 발표하는 부동산 지수를 다양한 챠트와 테이블로 아파트 주간 동향을 살펴 봄
+    - apt monthly: 한국부동산원과 KB에서 매달 발표하는 부동산 지수를 다양한 챠트와 테이블로 아파트 매월 동향을 살펴 봄
+    - local analysis: 시군 지역의 다양한 데이터를 확인할 수 있음. 예를 들면 인구수, 직장수, 평균 연봉, 미분양, 전세가율 등등
+    - rebuild house: 네이버 부동산에서 가져온 전국의 아파트분양권, 재개발, 재건축 매물 위치와 정보 확인
+    ### See more 
     - Check out [기하급수적](https://blog.naver.com/indiesoul2)
     - Ask a question in my [e-mail]<indiesoul2@naver.com>
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
 """
 )
