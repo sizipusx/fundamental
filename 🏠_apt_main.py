5dbca446c96a557dc2c20ceaf67e871bc6f631b7
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-11-24 23:02:24 +0900

    email chaeck

--- 🏠_apt_main.py
+++ 🏠_apt_main.py
@@ -18,7 +18,7 @@ st.markdown(
     ### Want to learn more?
     - Check out [기하급수적](https://blog.naver.com/indiesoul2)
     - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in my [e-mail](indiesoul2@naver.com)
+    - Ask a question in my [e-mail]<indiesoul2@naver.com>
     ### See more complex demos
     - Use a neural net to [analyze the Udacity Self-driving Car Image
         Dataset](https://github.com/streamlit/demo-self-driving)
