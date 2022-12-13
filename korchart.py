1b1bf8e995c6f22be5aa43f620477262a3025925
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-12-13 16:38:48 +0900

    integer

--- korchart.py
+++ korchart.py
@@ -76,7 +76,7 @@ def run(code, com_name):
     fig = go.Figure(go.Indicator(
         mode = "gauge+number+delta",
         value = value_df.iloc[13,0],
-        delta = {'reference': value_df.iloc[3,0], 'relative': True},
+        delta = {'reference': value_df.iloc[3,0].astype(int), 'relative': True},
         title = {'text': f"RIM-Price(r={r_ratio}) & 기대수익률"},
         domain = {'x': [0, 1], 'y': [0, 1]}
     ))
