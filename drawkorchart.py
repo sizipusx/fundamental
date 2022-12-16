97ecb8b7836d2ea9120a53f685dea4a56b6e05f0
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-12-16 16:45:40 +0900

    tkdkfk

--- drawkorchart.py
+++ drawkorchart.py
@@ -46,12 +46,12 @@ def income_chart(input_ticker, income_df, income_df_q):
             y_data_line = ['영업이익률', '순이익률', 'ROE(지배주주)']
 
             for y_data, color in zip(y_data_bar, marker_colors) :
-                fig.add_trace(go.Bar(name = y_data, x = income_df.index, y = income_df.loc[:,y_data], 
+                fig.add_trace(go.Bar(name = y_data, x = income_df.index.str, y = income_df.loc[:,y_data], 
                                             text= income_df[y_data], textposition = 'inside', marker_color= color), secondary_y = False) 
             
             for y_data, color in zip(y_data_line, marker_colors): 
                 fig.add_trace(go.Scatter(mode='lines+markers+text', 
-                                            name = y_data, x =  income_df.index, y= income_df.loc[:,y_data],
+                                            name = y_data, x =  income_df.index.str, y= income_df.loc[:,y_data],
                                             text= income_df[y_data], textposition = 'top center', marker_color = color),
                                             secondary_y = True)
             #fig.update_traces(texttemplate='%{text:.3s}') 
