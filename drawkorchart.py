6bc71ccc9cf77853d6492460f68d9aefc19c1b48
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-12-17 15:19:42 +0900

    roe 수정

--- drawkorchart.py
+++ drawkorchart.py
@@ -73,7 +73,7 @@ def income_chart(input_ticker, income_df, income_df_q):
             titles = dict(text= title, x=0.5, y = 0.85) 
             fig = make_subplots(specs=[[{'secondary_y': True}]]) 
             y_data_bar = ['매출액', '영업이익', '당기순이익']
-            y_data_line = ['영업이익률', '순이익률', 'ROE(지배주주)']
+            y_data_line = ['영업이익률', '순이익률', 'ROE']
 
             for y_data, color in zip(y_data_bar, marker_colors) :
                 fig.add_trace(go.Bar(name = y_data, x = income_df_q.index, y = income_df_q.loc[:,y_data], 
