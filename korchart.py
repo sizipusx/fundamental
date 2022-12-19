922cf96c6d0971ebb09ab45e2edeebfe69ae7821
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-12-19 16:24:29 +0900

    에러 포맷 수정

--- korchart.py
+++ korchart.py
@@ -267,8 +267,8 @@ def run(code, com_name):
         drawkorchart.income_chart(code, com_name, fn_ann_df.T, fn_qu_df.T, sep_flag)
         drawkorchart.balance_chart(code, com_name, fn_qu_df.T)
     except TypeError as te :
-        st.write("알수 없는 Error로 차트를 그릴 수 없습니다!")
-        st.error("Error 내용: ", te)
+        st.error("알수 없는 Error로 차트를 그릴 수 없습니다!", icon="🚨")
+        st.write("Error 내용: "+ te)
         
 if __name__ == "__main__":
     data_load_state = st.text('Loading KRX Company List...')
