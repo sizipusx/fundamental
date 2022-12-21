d324fefbe851c4cbbe4feb8756238ca2334b29e8
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-12-21 13:10:58 +0900

    fnguide 재무제표, 상태표, 현금흐름표 모두 가져오기

--- getData.py
+++ getData.py
@@ -583,5 +583,31 @@ def get_fdata_fnguide(firm_code):
     qu_df.index.name = '항목'
 
   return sep_flag, ann_df, qu_df, fs_tables
+
+  def get_html_fnguide(ticker,gb):
+    """    
+    :param ticker: 종목코드 
+    :param gb: 데이터 종류 (0 : 재무제표, 1 : 재무비율, 2: 투자지표)
+    :return: 
+    """
+    url=[]
+
+    url.append("https://comp.fnguide.com/SVO2/ASP/SVD_Finance.asp?pGB=1&gicode=A" + ticker + "&cID=&MenuYn=Y&ReportGB=&NewMenuID=103&stkGb=701")
+    url.append("https://comp.fnguide.com/SVO2/ASP/SVD_FinanceRatio.asp?pGB=1&gicode=A" + ticker + "&cID=&MenuYn=Y&ReportGB=&NewMenuID=104&stkGb=701")
+    url.append("https://comp.fnguide.com/SVO2/ASP/SVD_Invest.asp?pGB=1&gicode=A"+ ticker + "&cID=&MenuYn=Y&ReportGB=&NewMenuID=105&stkGb=701")
+
+    if gb>2 :
+        return None
+
+    url = url[gb]
+    try:
+
+        req = Request(url,headers={'User-Agent': 'Mozilla/5.0'})
+        html_text = urlopen(req).read()
+
+    except AttributeError as e :
+        return None
+
+    return html_text
   
 
