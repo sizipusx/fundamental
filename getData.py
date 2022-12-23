33a718ca01fd3bfba42f6cee96f5b60fd54bf257
indiesoul2@gmail.com <sizipusx2@gmail.com>
2022-12-23 11:26:46 +0900

    인덱스 순서 변경

--- getData.py
+++ getData.py
@@ -565,8 +565,8 @@ def make_Valuation(firm_code, firm_name, bond_y):
   # print("step 18. 총년수 END ==========================")
   onedf = pd.DataFrame(index=["종목코드", "종목명", "평가일","현재주가", "BPS", "PBR", "EPS(ttm)", "ttmPER", "DPS(MRY)","ROE","요구수익률","배당수익률","시가수익률", "r","ROE/r","적정주가(RIM)","패리티", "기대수익률", \
      "적정PBR", "5년 연평균수익률", "PBR갭수익률", "지속가능기간","컨센서스","컨센기업수","5년PER","5년PBR","PERR","PBRR","PER/PBR평균"], data=datalist)
-  #컬럼 순서 변경
-  onedf = onedf[["종목코드", "종목명", "평가일","현재주가", "적정주가(RIM)", "컨센서스", "기대수익률(RIM)", "5년 연평균수익률", "PBR갭수익률", "배당수익률","시가수익률", "지속가능기간",\
+  #인덱스 순서 변경
+  onedf.index = ["종목코드", "종목명", "평가일","현재주가", "적정주가(RIM)", "컨센서스", "기대수익률(RIM)", "5년 연평균수익률", "PBR갭수익률", "배당수익률","시가수익률", "지속가능기간",\
      "BPS", "PBR", "적정PBR", "EPS(ttm)", "ttmPER", "DPS(MRY)","ROE","요구수익률","r","ROE/r", "패리티", "컨센기업수","5년PER","5년PBR","PERR","PBRR","PER/PBR평균"]]
   #onedf.iloc[:,3:7] = onedf.iloc[:,3:7].apply(lambda int_num: '{:,}'.format(int_num))
 
