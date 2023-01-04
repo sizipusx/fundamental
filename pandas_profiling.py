16db7d770ef46196e98761791e622148c59d115f
indiesoul2@gmail.com <sizipusx2@gmail.com>
2023-01-04 22:39:36 +0900

    ㅇㄹㅇㄹㅇㄹㅇㄹ

--- pandas_profiling.py
+++ pandas_profiling.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import streamlit as st
 import pandas_profiling
+import streamlit as st
+
 
 uploaded_file = st.file_uploader('Choose a file')
 if uploaded_file is not None:
@@ -9,12 +10,13 @@ if uploaded_file is not None:
     try:
         #read xls or xlsx
         df=pd.read_excel(uploaded_file)
+        st.write('Data uploaded successfully. These are the first 5 rows.')
+        st.dataframe(df.head(5))
+        pr = df.profile_report()
+        st_profile_report(pr)
     except Exception as e:
             st.write(e)
 else:
     st.warning("you need to upload a csv or excel file.")
 
-st.write('Data uploaded successfully. These are the first 5 rows.')
-st.dataframe(df.head(5))
-pr = df.profile_report()
-st_profile_report(pr)
+
