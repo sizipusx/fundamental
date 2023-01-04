0028f8920b749771b425e25f403f637eea235bc9
indiesoul2@gmail.com <sizipusx2@gmail.com>
2023-01-04 22:51:53 +0900

    ffg

--- pandas_profiling.py
+++ pandas_profiling.py
@@ -1,7 +1,12 @@
 import pandas as pd
-import pandas_profiling
+import pandas_profiling 
 import streamlit as st
 
+from streamlit_pandas_profiling import st_profile_report
+
+@st.cache(allow_output_mutation=True)
+def gen_profile_report(df, *report_args, **report_kwargs):
+    return df.profile_report(*report_args, **report_kwargs)
 
 uploaded_file = st.file_uploader('Choose a file')
 if uploaded_file is not None:
@@ -12,8 +17,10 @@ if uploaded_file is not None:
         df=pd.read_excel(uploaded_file)
         st.write('Data uploaded successfully. These are the first 5 rows.')
         st.dataframe(df.head(5))
-        pr = df.profile_report()
-        st_profile_report(pr)
+        pr = gen_profile_report(df, explorative=True)
+
+        with st.expander("REPORT", expanded=True):
+            st_profile_report(pr)
     except Exception as e:
             st.write(e)
 else:
