d76c9b0f5c9bb498e79f863d2f38c64da1a0eeb6
indiesoul2@gmail.com <sizipusx2@gmail.com>
2023-01-16 12:15:45 +0900

    ㅀㅀ

--- pages/4 🏗️_apt_rebuild.py
+++ pages/4 🏗️_apt_rebuild.py
@@ -215,7 +215,7 @@ def load_data():
 def show_total(s_df):
     st.dataframe(s_df)
     px.set_mapbox_access_token(token)
-    fig = px.scatter_mapbox(s_df, lat="위도", lon="경도", color="매물종류", size="거래가(만)", hover_name="단지명", hover_data=["물건수", "공급면적(평)"],\
+    fig = px.scatter_mapbox(s_df, lat="위도", lon="경도", color="매물종류", size="거래가(만)", hover_name="단지명", hover_data=["물건수", "공급면적(평)", "시도명"],\
                     color_continuous_scale=px.colors.cyclical.IceFire, height=1000, size_max=30, zoom=10)
     fig.update_layout(
         title='전국 재건축-재개발 분양권 아파트 시세',
@@ -292,7 +292,7 @@ if __name__ == "__main__":
     st.subheader(f"시세 조사 날짜: {last_date}" )
     tab1, tab2 = st.tabs(["🗺️ 지도", "🔣 통계"])
     with tab1:
-        show_total(s_df)
+        #show_total(s_df)
         city_list = s_df['시도명'].drop_duplicates().to_list()
         city_list.insert(0,'전국')
         #submit = st.sidebar.button('해당 지역만 보기')
