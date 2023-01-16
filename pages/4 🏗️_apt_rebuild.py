316a7b3536fa562de6f2689a8ee12a8cca7a351f
indiesoul2@gmail.com <sizipusx2@gmail.com>
2023-01-16 12:08:47 +0900

    ㅇㄹㅇㄹ

--- pages/4 🏗️_apt_rebuild.py
+++ pages/4 🏗️_apt_rebuild.py
@@ -215,7 +215,7 @@ def load_data():
 def show_total(s_df):
     st.dataframe(s_df)
     px.set_mapbox_access_token(token)
-    fig = px.scatter_mapbox(s_df, lat="위도", lon="경도", color="매물종류", size="거래가(만)", hover_name="단지명", hover_data=["물건수", "공급면적(평)", "시도명"],
+    fig = px.scatter_mapbox(s_df, lat="위도", lon="경도", color="매물종류", size="거래가(만)", hover_name="단지명", hover_data=["물건수", "공급면적(평)", "시도명"],\
                     color_continuous_scale=px.colors.cyclical.IceFire, height=1000, size_max=30, zoom=10)
     fig.update_layout(
         title='전국 재건축-재개발 분양권 아파트 시세',
