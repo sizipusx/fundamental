2a4cb5da5979969423e2a6b19538103e592110c4
indiesoul2@gmail.com <sizipusx2@gmail.com>
2023-01-28 19:48:28 +0900

    dfdfdfadfad

--- chart.py
+++ chart.py
@@ -2,8 +2,8 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import plotly.io as pio
 import FinanceDataReader as fdr
-
 import streamlit as st
 from datetime import datetime
 
