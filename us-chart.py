1aabb0493532de123f010c29c5d7aaa24d70c298
indiesoul2@gmail.com <sizipusx2@gmail.com>
2023-01-29 23:52:31 +0900

    dfdfdfaf

--- us-chart.py
+++ us-chart.py
@@ -738,9 +738,9 @@ if __name__ == "__main__":
     fdr_df['ma5'] = fdr_df['Adj Close'].rolling(window=5).mean()
     fdr_df['ma20'] = fdr_df['Adj Close'].rolling(window=20).mean()
     fdr_df['ma240'] = fdr_df['Adj Close'].rolling(window=240).mean()
-    #overview_df.loc['Close'] = overview_df.iloc[-1,4]
+    overview_df.loc['Close'] = fdr_df.iloc[-1,4]
     st.table(overview_df)
-    st.write(overview_df.iloc[-1,4])
+    # st.write(overview_df.iloc[-1,4])
     chart.price_chart(input_ticker, overview_df.loc['Name'], fdr_df)
     submit = st.sidebar.button('Run app')
     if submit:
