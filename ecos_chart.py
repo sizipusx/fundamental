8550be7d74ca6a72299cb479eb6265373ef348b1
indiesoul2@gmail.com <sizipusx2@gmail.com>
2023-03-03 23:00:16 +0900

    dfdfffsfdfsdf

--- ecos_chart.py
+++ ecos_chart.py
@@ -42,7 +42,8 @@ def ecos_monthly_chart(input_ticker, df1, df2):
             titles = dict(text= input_ticker, x=0.5, y = 0.85) 
             fig = make_subplots(specs=[[{'secondary_y': True}]]) 
             item_list = df1.columns.values.tolist()
-            y_data_bar, y_data_line = []
+            y_data_bar = []
+            y_data_line = []
             for item in item_list:
                 y_data_bar.append(item)
                 y_data_line.append(item)
@@ -56,12 +57,14 @@ def ecos_monthly_chart(input_ticker, df1, df2):
                                             name = y_data, x =  x_data, y= df1.loc[:,y_data],
                                             text= df1[y_data], textposition = 'top center', marker_color = color),
                                             secondary_y = False)
-            #fig.update_traces(texttemplate='%{text:.3s}') 
-            fig.update_yaxes(title_text='대출금액(조원)', range=[0, max(df1.loc[:,y_data_bar[0]])*1.2], secondary_y = False)
+            #fig.update_traces(texttemplate='%{text:.3s}')
+            if input_ticker == '가계신용': 
+                fig.update_yaxes(title_text='대출금액(조원)', range=[0, max(df1.loc[:,y_data_bar[0]])*1.2], zeroline=True, ticksuffix="조원", secondary_y = False)
+            else:
+                fig.update_yaxes(title_text='input_ticker', range=[0, max(df1.loc[:,y_data_bar[0]])*1.2], secondary_y = False)
             #fig.update_yaxes(title_text='Profit', range=[0, max(income_df.loc[:,y_data_bar[0]])*2], secondary_y = False)
             fig.update_yaxes(title_text='전월대비증감', range=[-max(df2.loc[:,y_data_line[0]]), max(df2.loc[:,y_data_line[0]])* 1.2], secondary_y = True)
             fig.update_yaxes(showticklabels= True, showgrid = False, zeroline=True, ticksuffix="%", secondary_y = True)
-            fig.update_yaxes(showticklabels= True, showgrid = False, zeroline=True, ticksuffix="조원", secondary_y = False)
             fig.update_layout(title = titles, titlefont_size=15, legend=dict(orientation="h"), template=template, xaxis_tickformat = '%Y.%m')
             fig.update_layout(template="myID")
             st.plotly_chart(fig)
