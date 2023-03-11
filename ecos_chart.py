eda903c0903f99a05cc0bab6832c0670b39e35e4
indiesoul2@gmail.com <sizipusx2@gmail.com>
2023-03-11 13:29:25 +0900

    color change

--- ecos_chart.py
+++ ecos_chart.py
@@ -122,11 +122,11 @@ def fred_monthly_chart(ticker, kor_exp, df):
             y_data_bar = [mom_df.columns[0]]
             y_data_line= [df.columns[0]]
 
-            for y_data, color in zip(y_data_bar, marker_colors) :
+            for y_data, color in zip(y_data_bar, marker_colors1) :
                 fig.add_trace(go.Bar(name = y_data, x = x_data, y = mom_df.loc[:,y_data], 
                                             text= mom_df[y_data], textposition = 'inside', marker_color= color), secondary_y = True) 
             
-            for y_data, color in zip(y_data_line, marker_colors): 
+            for y_data, color in zip(y_data_line, marker_colors1): 
                 fig.add_trace(go.Scatter(mode='lines+markers', 
                                             name = y_data, x =  x_data, y= df.loc[:,y_data],
                                             marker_color = color),
