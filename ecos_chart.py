3bc586578a212f00500db94d19d9dd1f6a1dc76b
기하급수적 <sizipusx2@gmail.com>
2023-05-27 15:08:25 +0900

    dfdff

--- ecos_chart.py
+++ ecos_chart.py
@@ -464,7 +464,7 @@ def ecos_spread_chart(input_ticker, df1):
             y_data_bar = [df1.columns[4]]
             y_data_line = [df1.columns[1], df1.columns[2]]
             y_data_color = [df1.columns[6]]
-            for y_data, color in zip(y_data_bar, marker_colors2) :
+            for y_data, color in zip(y_data_bar, y_data_color) :
                 fig.add_trace(go.Bar(name = y_data, x = x_data, y = df1.loc[:,y_data], 
                                             text= df1[y_data], textposition = 'inside', marker_color= df1.loc[:,color]), secondary_y = True) 
             
