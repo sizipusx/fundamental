6e014bd7089dd1ef19b50e84699bcb7eea2d3c46
기하급수적 <sizipusx2@gmail.com>
2023-07-25 12:55:18 +0900

    ㄴㄴㄴㄹ

--- pages/4 🏗️_apt_rebuild.py
+++ pages/4 🏗️_apt_rebuild.py
@@ -335,19 +335,19 @@ if __name__ == "__main__":
         # filter_df.loc[:, numeric_columns] = filter_df.loc[:, numeric_columns].apply(pd.to_numeric, errors='coerce')
 
 
-        if response:
-            st.write("선택한 아파트:")
-            selected_df = response["selected_rows"]
-            if selected_df:
-                px.set_mapbox_access_token(token)
-                fig = px.scatter_mapbox(selected_df, lat="위도", lon="경도", color="매물종류", size='공급면적', hover_name="단지명", hover_data=["특이사항", "한글거래가액", "시도명"],
-                                color_continuous_scale=px.colors.cyclical.IceFire, size_max=30, zoom=10, height=500)
-                fig.update_layout(
-                    title='선택한 아파트 네이버 시세',
-                    legend=dict(orientation="h")
-                )
-                fig.update_layout(mapbox_style="satellite-streets")
-                st.plotly_chart(fig, use_container_width=True)
+        # if response:
+        #     st.write("선택한 아파트:")
+        #     selected_df = response["selected_rows"]
+        #     if selected_df:
+        #         px.set_mapbox_access_token(token)
+        #         fig = px.scatter_mapbox(selected_df, lat="위도", lon="경도", color="매물종류", size='공급면적', hover_name="단지명", hover_data=["특이사항", "한글거래가액", "시도명"],
+        #                         color_continuous_scale=px.colors.cyclical.IceFire, size_max=30, zoom=10, height=500)
+        #         fig.update_layout(
+        #             title='선택한 아파트 네이버 시세',
+        #             legend=dict(orientation="h")
+        #         )
+        #         fig.update_layout(mapbox_style="satellite-streets")
+        #         st.plotly_chart(fig, use_container_width=True)
 
                 #folium에 표시에 보자
                 # df = pd.DataFrame(selected_df)
