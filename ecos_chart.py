ae2964041f1029c4c55a0d9ea52177b82b105911
기하급수적 <sizipusx2@gmail.com>
2023-10-25 14:58:14 +0900

    dfedf

--- ecos_chart.py
+++ ecos_chart.py
@@ -64,8 +64,8 @@ def ecos_debt_chart(input_ticker, df1, df2):
             y_data_line.append(item_list[3])
 
             for y_data, color in zip(y_data_bar, marker_colors1) :
-                fig.add_trace(go.Bar(name = y_data+'(R)', x = x_data, y = df1.loc[:,y_data], marker_color= color), secondary_y = True) 
-                                            # text= df1.loc[:,y_data], textposition = 'inside', marker_color= color, secondary_y = True)) 
+                fig.add_trace(go.Bar(name = y_data+'(R)', x = x_data, y = df1.loc[:,y_data], marker_color= color,
+                                            text= df1.loc[:,y_data], textposition = 'inside', marker_color= color), secondary_y = True)
             
             for y_data, color in zip(y_data_line, marker_colors2): 
                 fig.add_trace(go.Scatter(mode='lines', 
@@ -135,8 +135,8 @@ def ecos_debt_chart(input_ticker, df1, df2):
             y_data_line.append(item_list[3])
 
             for y_data, color in zip(y_data_bar, marker_colors1) :
-                fig.add_trace(go.Bar(name = y_data+'(R)', x = x_data, y = df2.loc[:,y_data], marker_color= color), secondary_y = True)
-                                            # text= df2[y_data], textposition = 'inside', marker_color= color), secondary_y = True)
+                fig.add_trace(go.Bar(name = y_data+'(R)', x = x_data, y = df2.loc[:,y_data], marker_color= color,
+                                            text= df2[y_data], textposition = 'inside', marker_color= color), secondary_y = True)
             
             for y_data, color in zip(y_data_line, marker_colors2): 
                 fig.add_trace(go.Scatter(mode='lines', 
