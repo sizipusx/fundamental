29b31aea55c6426ce5744c140fc355f9f87b3d87
기하급수적 <sizipusx2@gmail.com>
2023-10-25 12:50:50 +0900

    dfdfdfdf

--- ecos_chart.py
+++ ecos_chart.py
@@ -57,9 +57,9 @@ def ecos_debt_chart(input_ticker, df1, df2):
             fig = make_subplots(specs=[[{'secondary_y': True}]]) 
             y_data_bar = []
             y_data_line = []
-            for item in item_list[:2]:
+            for item in item_list[:3]:
                 y_data_bar.append(item)
-            y_data_line.append(item_list[2])
+            y_data_line.append(item_list[3])
 
             for y_data, color in zip(y_data_bar, marker_colors1) :
                 fig.add_trace(go.Bar(name = y_data+'(R)', x = x_data, y = df1.loc[:,y_data], 
