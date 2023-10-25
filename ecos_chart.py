4ffdbf6704a8eb1c1576e62c07a8cf21ac80cbb7
기하급수적 <sizipusx2@gmail.com>
2023-10-25 13:03:55 +0900

    sss

--- ecos_chart.py
+++ ecos_chart.py
@@ -60,18 +60,17 @@ def ecos_debt_chart(input_ticker, df1, df2):
             y_data_bar = []
             y_data_line = []
             for item in item_list[:3]:
-                st.write(item)
                 y_data_bar.append(item)
             y_data_line.append(item_list[3])
 
             for y_data, color in zip(y_data_bar, marker_colors1) :
                 fig.add_trace(go.Bar(name = y_data+'(R)', x = x_data, y = df1.loc[:,y_data], 
-                                            text= df1[y_data], textposition = 'inside', marker_color= color, secondary_y = True)) 
+                                            text= df1.loc[:,y_data], textposition = 'inside', marker_color= color, secondary_y = True)) 
             
             for y_data, color in zip(y_data_line, marker_colors2): 
                 fig.add_trace(go.Scatter(mode='lines', 
                                             name = y_data+'(L)', x =  x_data, y= df1.iloc[:,-1],
-                                            text= df1[y_data], textposition = 'top center', marker_color = color),
+                                            text= df1.loc[:,y_data], textposition = 'top center', marker_color = color),
                                             secondary_y = False)
             fig.update_layout(barmode='stack')
             fig.update_yaxes(title_text='대출금액(조원)', range=[0, max(df1.loc[:,y_data_bar[0]])*1.2], zeroline=True, ticksuffix="조원", secondary_y = False)
