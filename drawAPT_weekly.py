7f9b70788e7074cc3788bf6f2863c47f93405588
기하급수적 <sizipusx2@gmail.com>
2025-01-04 14:11:00 +0900

    ㄴㄴㄴㅇㅇ

--- drawAPT_weekly.py
+++ drawAPT_weekly.py
@@ -289,7 +289,7 @@ def draw_momentum_with_bar(last_df, flag, last_week):
     fig.update_layout(uniformtext_minsize=6, uniformtext_mode='show')
     fig.update_xaxes(title_text=flag[1], showticklabels= True, showgrid = True, zeroline=True, zerolinecolor='LightPink', ticksuffix="%")
     fig.update_layout(template="myID")
-    st.plotly_chart(fig, key=f"momentum_bubble_chart_{flag}_{last_week}")
+    st.plotly_chart(fig, key=f"momentum_bubble_chart_{flag[0]}_{flag[1]}_{last_week}")
 
 
 def draw_index_change_with_bubble(last_df, flag, last_week):
