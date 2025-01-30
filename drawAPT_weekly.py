2abef52b559a7a3983ca44a8e95e5e8668474ccc
기하급수적 <sizipusx2@gmail.com>
2025-01-30 20:58:55 +0900

    Add region mapping for index modification in draw_senti_last function

--- drawAPT_weekly.py
+++ drawAPT_weekly.py
@@ -845,6 +845,28 @@ def draw_change_table(change_df,flag):
     st.plotly_chart(fig)
 
 def draw_senti_last(to_df, last_week):
+    # 매핑 테이블 예시
+    region_mapping = {
+        "서울특별시": "서울",
+        "부산광역시": "부산",
+        "대구광역시": "대구",
+        "인천광역시": "인천",
+        "광주광역시": "광주",
+        "대전광역시": "대전",
+        "울산광역시": "울산",
+        "세종특별자치시": "세종",
+        "경기도": "경기",
+        "강원특별자치도": "강원",
+        "충청북도": "충북",
+        "충청남도": "충남",
+        "전북특별자치도": "전북",
+        "전라남도": "전남",
+        "경상북도": "경북",
+        "경상남도": "경남",
+        "제주특별자치도": "제주"
+    }
+    # 인덱스 수정
+    to_df.index = to_df.index.map(lambda x: region_mapping.get(x, x))
     custom_color_scale = [
         [0, "blue"],   # Dark blue for the most negative value
         [0.5, "white"],  # White for zero
